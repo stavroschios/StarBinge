4a46a8af8e73542c746d24198cf590876f7f0484
Stavros <stavroschios@gmail.com>
2020-07-15 22:49:40 -0400

    Initial commit

---
+++ src/star.java
@@ -0,0 +1,24 @@
+
+public class star {
+    public static void main(String[] args) {
+
+        printSquareStar(8);
+    }
+    public static void printSquareStar(int number) {
+
+            if (number > 4) {
+
+                for (int i = 0; i < number; i++) {
+                    for (int j = 0; j < number; j++) {
+                        if (i == 0 || i == (number - 1) || j == 0 || j == (number -1) || i == j || i == (number - j - 1)) {
+                            System.out.print("*");
+                        } else System.out.print(" ");
+                    }
+                    System.out.println();
+                }
+
+            } else System.out.println("Invalid Value");
+        }
+    }
+
+
